61dc5c27e6541c38cf23554b90abb7661c5e781d
Marcin Kumorek <kumeek@gmail.com>
2020-10-18 21:15:15 +0200

    add backend stuff...

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import Amplify from 'aws-amplify'
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+import awsConfig from './aws-exports'
+
+Amplify.configure(awsConfig)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn how to use AWS AppSync :)
-        </a>
-      </header>
+    <div>
+      <header>Learn how to use AWS AppSync :)</header>
+      <main>
+        Main content
+        <AmplifySignOut />
+      </main>
     </div>
   )
 }
 
-export default App
+export default withAuthenticator(App)
